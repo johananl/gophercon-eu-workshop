ead535131cb28e7bace52081100b5a5d13398f17
Johanan Liebermann <johanan.liebermann@gmail.com>
2018-05-31 10:58:34 +0000

    Move routing to separate package

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/johananl/gophercon-eu-workshop/pkg/routing"
 )
 
 func main() {
 	log.Printf("Service is starting...")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/home", homeHandler()).Methods(http.MethodGet)
+	r := routing.BaseRouter()
 
 	http.ListenAndServe(":8080", r)
-}
-
-func homeHandler() func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Request is processing: %s", r.URL.Path)
-		fmt.Fprint(w, "Hello! Your request was processed.")
-	}
 }
